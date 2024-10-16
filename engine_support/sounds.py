3d4279006b991abd0ef2b0d55ecd97434a65ed55
Ionut <ionut.riciu92@gmail.com>
2024-10-16 18:33:37 +0300

    Working audio whit files in the base path

--- engine_support/sounds.py
+++ engine_support/sounds.py
@@ -2,11 +2,3 @@ sound_main_music = 'looping_music.ogg'
 laser_sound = 'laserfire02.ogg'
 sound_explosion = 'explosions.flac'
 
-
-# sound_main_music = 'assets','audio','game_music','looping_music.ogg'
-# laser_sound = 'assets','audio','laserfire02.ogg'
-# sound_explosion = 'assets','audio','laser','explosion','explosions.flac'
-
-# sound_main_music = 'assets_1','audio_fix','looping_music.ogg'
-# laser_sound = 'laserfire02.ogg'
-# sound_explosion = 'explosions.flac'
